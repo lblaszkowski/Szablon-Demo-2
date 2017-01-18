59180ded1e16e85d2d9c74c5fa894e0d25c832c7
Lukasz Blaszkowski <lblaszkowski@onet.pl>
2017-01-18 21:39:26 +0100

    Szablon strony www

---
+++ skrypt.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	
+	$("#obramowanie ul li img").mouseover(function() {
+		$(this).css("opacity", "0.6");
+	}).mouseout(function() {
+		$(this).css("opacity", "1.0");
+	});
+	
+	
+});
